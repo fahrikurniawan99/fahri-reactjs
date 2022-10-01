1560e647665621a63d75e85e330cc70cb60d199b
fahrikurniawan99 <fahrikurniawan954@gmail.com>
2022-10-01 07:29:57 +0700

    Mengubah isi App.js

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class index extends Component {
+export default class Component extends Component {
   render() {
     return (
       <div>index</div>
