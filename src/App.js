57ca90ca6d5ab896a16f4f1d3417729f507b62a5
fahrikurniawan99 <fahrikurniawan954@gmail.com>
2022-10-28 09:13:05 +0700

    tugas react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import TugasHooks from "./Tugas-Hooks";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className='vstack align-items-center'>
       {/* <Component/> */}
       {/* <TugasPortofolio/> */}
       {/* <PortalBerita /> */}
-      <TugasHooks />
+      {/* <TugasHooks /> */}
+      <div className='py-3'>
+        <Link to='home' className='btn btn-outline-primary'>
+          Home
+        </Link>
+        <Link to='product' className='btn btn-outline-success mx-3'>
+          Product
+        </Link>
+        <Link to='about' className='btn btn-outline-dark'>
+          About
+        </Link>
+      </div>
+      <div>
+        <div className='vh-100 hstack'>
+        <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import About from "./Tugas-ReactRouter/About";
+import Detail from "./Tugas-ReactRouter/Detail";
+import Home from "./Tugas-ReactRouter/Home";
+import Product from "./Tugas-ReactRouter/Product";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='home' element={<Home />} />
+          <Route path='product' element={<Product />} />
+          <Route path='product/detail/:name' element={<Detail />} />
+          <Route path='about' element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
