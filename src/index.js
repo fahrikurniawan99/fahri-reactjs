4462680adce1dc95dd88ad16715c2aa46f7e2dd7
fahrikurniawan99 <fahrikurniawan954@gmail.com>
2022-10-28 09:59:13 +0700

    Fix module not found

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Hello from './Test/Hello';
+import About from './TugasReactRouter/About';
+import Detail from './TugasReactRouter/Detail';
+import Home from './TugasReactRouter/Home';
+import Product from './TugasReactRouter/Product';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />}>
           <Route path='home' element={<Home />} />
@@ -17,8 +22,7 @@ root.render(
           <Route path='about' element={<About />} />
         </Route>
       </Routes>
-    </BrowserRouter> */}
-    <Hello />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
