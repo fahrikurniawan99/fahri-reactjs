025bc8baf8a3a991e4ec7ab29e1266eb9eeb833b
fahrikurniawan99 <fahrikurniawan954@gmail.com>
2022-10-28 09:53:36 +0700

    Test2

--- src/Test/Hello.js
+++ src/Test/Hello.js
@@ -1,7 +1,5 @@
-import React from 'react'
-
-export const Hello = () => {
-  return (
-    <div>Hello</div>
-  )
-}
+import React from "react";
+const Hello = () => {
+  return <div>Hello</div>;
+};
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { Hello } from "./Test";
+import Hello from './Test/Hello';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
